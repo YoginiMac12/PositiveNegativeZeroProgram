57ada254794d9fb4c82aaa19cb7f83582b4be9f6
YoginiMac12 <113415806+YoginiMac12@users.noreply.github.com>
2022-09-19 23:19:44 +0530

    Add files via upload

---
+++ PositiveNegativeZeroProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositiveNegativeZeroProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a Number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n > 0)
+            { 
+                Console.WriteLine("Number is Postive");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine("Number is Negative");
+            }
+            else
+            {
+                Console.WriteLine("Number is Zero");
+            }
+            Console.ReadLine();
+        }
+    }
+}
